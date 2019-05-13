a851542c4765f4ee50ac85c5cb6ac51f3257645e
Jason Leach <jlspursfan@gmail.com>
2019-05-13 01:18:14 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,7 @@
 def reverse_each_word(str)
   arr = str.split(" ")
-  new_arr = []
-  arr.each do |word|
-    new_arr << word.reverse!
+  arr.collect do |word|
+    word.reverse!
   end
-  new_arr.join(" ")
+  arr.join(" ")
 end
